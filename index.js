4d9eaf14f68d8be5bdba8b62825cb0b93a59939b
Stilla <sunnatilla95@gmail.com>
2024-02-15 08:37:48 +0500

    branch open

---
+++ index.js
@@ -0,0 +1,11 @@
+// setTimeout(() => {
+//     console.log("Sunnatilla");
+// }, 1000); 
+document.addEventListener('DOMContentLoaded', function () {
+    // Код, который должен выполниться после полной загрузки DOM
+    console.log("DOM fully loaded.");
+
+    setTimeout(() => {
+        console.log("Sunnatilla");
+    }, 1000);
+});
